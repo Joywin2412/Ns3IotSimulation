17ce3b8c1a702c31820c45f3d8214c488acbf1b7
= <joywinpinto1@gmail.com>
2024-02-25 22:08:54 +0530

    Ns3 started

---
+++ iot.cc
@@ -0,0 +1,14 @@
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/lr-wpan-module.h"
+#include "ns3/mobility-module.h"
+
+using namespace ns3;
+
+int main (int argc, char *argv[])
+{
+  // Create nodes
+ 
+  return 0;
+}
